f074fadd507acdfc507d980fe8038b3428968b92
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-11 10:32:58 -0400

    chore(config):add prettier formatting config

---
+++ frontend/prettier.config.js
@@ -0,0 +1,10 @@
+/**@type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  arrowParens: "always",
+}
+
+export default config
