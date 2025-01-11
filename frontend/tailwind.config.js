abc486041cf94e9e7e9b1cdcfd38aa0c5a134943
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-11 13:22:05 -0400

    feat(styles):add css variables & map on tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,42 @@
+import { fontFamily } from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...fontFamily.sans],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          focus: "hsl(var(--primary-focus))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        wpp: {
+          DEFAULT: "hsl(var(--wpp))",
+          focus: "hsl(var(--wpp-focus))",
+          foreground: "hsl(var(--wpp-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          medium: "hsl(var(--muted-medium))",
+          low: "hsl(var(--muted-low))",
+        },
+      },
+    },
   },
   plugins: [],
 }
