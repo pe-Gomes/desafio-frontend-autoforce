fa5c1f2ec9ca49f49b990b8ce12891cea8b92110
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-13 15:42:59 -0400

    feat(db):add seed with one company & vehicle

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Company.create!({
+  company_name: "AUTO FORCE PLATAFORMA DE MARKETING DIGITAL LTDA",
+  cnpj: "22.580.947/0001-06",
+  address: "Av. Prudente de Morais, 3966, Lagoa Nova, Natal/RN – CEP 59056-200",
+  phone: "84987654321",
+  whatsapp: "84912345678"
+})
+
+puts "Company created!"
+
+Vehicle.create!({
+  name: "BMW Série 4 Cabrio",
+  price: 179950,
+  image: "https://imgur.com/gnISsZt",
+  video: "https://www.youtube.com/watch?v=Fr5lt86dPyU",
+  legal_text: "Valor de tabela (código 823839), ano/modelo 18/19 à vista a partir de R$ 179.950 ou financiado com entrada de R$ 40.490 (40%) e mais 24 prestações mensais de R$ 1.286, taxa de juros 0% a.m. e 0% a.a. Total da operação: R$ 205.874. Oferta válida para veículos com pintura sólida.",
+  consumption: "14.7 km/l",
+  engine: "999 cm3",
+  power: "75 cv",
+  speed: "154 km/h"
+})
+
+puts "Vehicle created!"
