8bdfc6b2b74b806dab12c6ff33228609486eca36
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-12 20:54:17 -0400

    refactor: update index styles & main component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./index.css"
+
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { Header } from "@/components/header"
+import { Footer } from "./components/footer"
+import { Home } from "./pages/home"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>Hello, World!</div>
+    <div className='flex min-h-screen flex-col'>
+      <Header />
+      <main className='flex-grow'>
+        <Home />
+      </main>
+      <Footer />
+    </div>
   </StrictMode>,
-);
+)
