12c0973b876eafa722f24dbcc8ecbae1ba931ed8
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-13 16:23:07 -0400

    feat(model):add company schema validation & type

---
+++ frontend/src/models/company.ts
@@ -0,0 +1,14 @@
+import { z } from "zod"
+
+export const companySchema = z.object({
+  id: z.string(),
+  company_name: z.string(),
+  cnpj: z.string(),
+  adress: z.string(),
+  phone: z.string(),
+  whatsapp: z.string(),
+  created_at: z.date(),
+  updated_at: z.date(),
+})
+
+export type Company = z.infer<typeof companySchema>
